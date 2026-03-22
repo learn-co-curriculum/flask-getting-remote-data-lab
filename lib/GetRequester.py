c542a1e8d80ce9aa68122619ea9065864132ff73
bakari <anbphones@icloud.com>
2026-03-22 09:03:38 +0300

    api added

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,24 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response
 
     def load_json(self):
-        pass
+        response = self.get_response_body()
+        return response.json()
+    
+if __name__ == "__main__":
+    url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    requester = GetRequester(url)
+
+    # Get raw response
+    response = requester.get_response_body()
+    if response:
+        print(f"Status code: {response.status_code}")
+
+    # Get JSON data
+    data = requester.load_json()
+    if data:
+        print("JSON data received:")
+        print(data)
