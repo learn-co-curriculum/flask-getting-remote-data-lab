46bec58e88ce991dd3e491b17f72fb0b93a90587
Ryan Rooney <manythanks089@gmail.com>
2026-06-13 19:43:13 -0400

    Completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -5,9 +5,14 @@ class GetRequester:
 
     def __init__(self, url):
         self.url = url
+        self.response = None  # store response for reuse
 
     def get_response_body(self):
-        pass
+        self.response = requests.get(self.url)
+        return self.response.text
 
     def load_json(self):
-        pass
+        if self.response is None:
+            self.get_response_body()
+
+        return self.response.json()
