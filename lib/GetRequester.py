a82fedc0d96bfa207ab2aa9149d6b2e1c62fe8b5
CynthiaG-W <cindydetec@gmail.com>
2026-06-28 12:30:19 +0300

    Fix GetRequester methods

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -8,14 +8,11 @@ class GetRequester:
         self.response = None
 
     def get_response_body(self):
-        # Send request and store response
         self.response = requests.get(self.url)
-        return self.response.content  # bytes, not text
+        return self.response.content   # MUST return bytes
 
     def load_json(self):
-        # Ensure response exists
         if self.response is None:
             self.get_response_body()
 
-        # Convert JSON bytes/string into Python list
         return json.loads(self.response.content)
