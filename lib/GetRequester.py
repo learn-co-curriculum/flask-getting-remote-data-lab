9bdad10036da271d1974b30950b1f734a43275bf
CynthiaG-W <cindydetec@gmail.com>
2026-06-28 12:24:31 +0300

    Complete GetRequester lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -5,9 +5,17 @@ class GetRequester:
 
     def __init__(self, url):
         self.url = url
+        self.response = None
 
     def get_response_body(self):
-        pass
+        # Send request and store response
+        self.response = requests.get(self.url)
+        return self.response.content  # bytes, not text
 
     def load_json(self):
-        pass
+        # Ensure response exists
+        if self.response is None:
+            self.get_response_body()
+
+        # Convert JSON bytes/string into Python list
+        return json.loads(self.response.content)
